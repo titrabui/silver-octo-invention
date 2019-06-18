6a8b6cd052abc76ea31156bd537dbc1c2467d668
titrabui <titrabui@gmail.com>
2019-06-19 00:08:56 +0700

    implement roles editing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_040524) do
+ActiveRecord::Schema.define(version: 2019_06_18_164747) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2019_06_14_040524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
 end
