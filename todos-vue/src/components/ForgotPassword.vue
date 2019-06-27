148c9e294097f8bbdc2f2bf9e3034f84e39b6e09
titrabui <titrabui@gmail.com>
2019-06-28 02:01:46 +0700

    finish redesign forgot password page

--- todos-vue/src/components/ForgotPassword.vue
+++ todos-vue/src/components/ForgotPassword.vue
@@ -1,18 +1,40 @@
 <template>
-  <form class="form-app form-forgot-password" @submit.prevent="submit">
-    <div class="alert alert-info" v-if="notice">{{ notice }}</div>
-    <div class="alert alert-danger" v-if="error">{{ error }}</div>
-    <div class="form-group">
-      <label for="email">Email address</label>
-      <input v-model="email" type="email" class="form-control" id="email" placeholder="email@example.com">
-    </div>
-    <button type="submit" class="btn btn-primary mb-3">Reset Password</button>
-    <div>
-      <router-link to="/">Sign in</router-link>
-      <br />
-      <router-link to="/signup">Sign up</router-link>
-    </div>
-  </form>
+  <el-row>
+    <el-row class="banner-image">
+      <img style="width: 100%; height: auto;" src="@/assets/images/banner_1.jpg">
+    </el-row>
+    <el-row class="page-title">
+      <span>Reset Password</span>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="6" :offset="9" class="signin-form">
+        <el-alert
+          v-if="notice"
+          :title="notice"
+          type="success"
+          effect="dark"
+          center
+          :closable=false>
+        </el-alert>
+        <el-alert
+          v-if="error"
+          :title="error"
+          type="error"
+          effect="dark"
+          center
+          :closable=false>
+        </el-alert>
+        <el-form :model="resetForm" ref="resetForm" :rules="resetValidates" label-position="top" label-width="100px" size="large" :hide-required-asterisk="true">
+          <el-form-item prop="email" label="Email address">
+            <el-input placeholder="Please input email" v-model="resetForm.email"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" style="width: 100%" round @click="reset()">Reset Password</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </el-row>
 </template>
 
 <script>
@@ -20,16 +42,30 @@ export default {
   name: 'ForgotPassword',
   data () {
     return {
-      email: '',
+      resetForm: {
+        email: ''
+      },
       error: '',
-      notice: ''
+      notice: '',
+      resetValidates: {
+        email: [
+          { required: true, message: 'Please input email address', trigger: 'blur' },
+          { type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change'] }
+        ]
+      }
     }
   },
   methods: {
-    submit () {
-      this.$http.plain.post('/password_resets', { email: this.email })
-        .then(() => this.submitSuccessful())
-        .catch(error => this.submitFailed(error))
+    reset () {
+      this.$refs['resetForm'].validate((valid) => {
+        if (valid) {
+          this.$http.plain.post('/password_resets', { email: this.resetForm.email })
+            .then(() => this.submitSuccessful())
+            .catch(error => this.submitFailed(error))
+        } else {
+          return false
+        }
+      })
     },
     submitSuccessful () {
       this.notice = 'Email with password reset instructions had been sent.'
@@ -42,3 +78,23 @@ export default {
   }
 }
 </script>
+<style lang="css">
+.signin-form {
+  padding: 20px 20px 0 !important;
+  border: dashed 1px #409EFF;
+  border-radius: 5px;
+  margin-top: 20px;
+  background-color: #F2F6FC;
+}
+
+.banner-image {
+  margin: -19px -20px 30px;
+}
+
+.page-title {
+  text-align: center;
+  font-weight: 500;
+  font-size: 25px;
+  color: #409EFF;
+}
+</style>
--- todos-vue/src/components/posts/Create.vue
+++ todos-vue/src/components/posts/Create.vue
@@ -6,7 +6,7 @@
           span.box-header-title {{ isUpdating() ? 'Update News' : 'Create a News' }}
           el-button(style="float: right" type="primary" icon="el-icon-back" round size="mini" @click="backHome()") BACK
         el-row(style="padding: 14px 20px")
-          el-form(ref="postForm" :rules="postValidates" :model="post" label-position="top" label-width="100px")
+          el-form(ref="postForm" :rules="postValidates" :model="post" label-position="top" label-width="100px" :hide-required-asterisk="true")
             el-form-item(label="Title" prop="title")
               el-input(placeholder="Please input title" v-model="post.title" maxlength="255" show-word-limit)
             el-form-item(label="Sub title" prop="sub_title")
